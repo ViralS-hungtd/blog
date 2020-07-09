9bafd443ccc3727f81f0f3964ec1dabaa2f011d3
Your Name <you@example.com>
2020-07-09 22:03:56 +0700

    quiz

--- resources/views/front/quiz.blade.php
+++ resources/views/front/quiz.blade.php
@@ -38,15 +38,31 @@
                             @endforeach
                                 <div class="carousel-item">
                                     <h4><b>Vui lòng để lại thông tin cá nhân của bạn để cập nhật những thông tin mới nhất từ CyberKid</b></h4>
-                                    <p><span>Họ và tên :   </span><input type="text" name="name_customer"></p>
-                                    <p style="color: red" class="validate validate_name_customer"></p>
-                                    <p><span>Nghề nghiệp : </span><input type="text" name="job_customer"></p>
-                                    <p style="color: red" class="validate validate_job_customer"></p>
-                                    <p><span>Email :       </span><input type="text" name="email_customer"></p>
-                                    <p style="color: red" class="validate validate_email_customer"></p>
-                                    <button type="submit" class="btn btn-result" aria-expanded="true">XEM KẾT QUẢ</button>
+                                    <div class="form-group row mt-3">
+                                        <label for="staticEmail" class="col-sm-3 col-form-label">Họ và tên : </label>
+                                        <div class="col-sm-7">
+                                            <input type="text" name="name_customer" class="form-control" placeholder="Họ và tên">
+                                        </div>
+                                        <small style="color: red" class="validate validate_name_customer"></small>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="inputPassword" class="col-sm-3 col-form-label">Nghề nghiệp :</label>
+                                        <div class="col-sm-7">
+                                            <input type="text" name="job_customer" class="form-control"  placeholder="ghề nghiệp">
+                                        </div>
+                                        <small style="color: red" class="validate validate_job_customer"></small>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="inputPassword" class="col-sm-3 col-form-label">Email :</label>
+                                        <div class="col-sm-7">
+                                            <input type="text" name="email_customer" class="form-control" placeholder="email@gmail.com">
+                                        </div>
+                                        <small style="color: red" class="validate validate_email_customer"></small>
+                                    </div>
+                                    <button type="submit" class="btn btn-result float-md-right" aria-expanded="true">XEM KẾT QUẢ</button>
                                 </div>
-                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">>>
+                            <a class="carousel-control-next" id="next-button" href="#carouselExampleControls" role="button" data-slide="next">
+                                <img src="{{ asset('assets/images/arrow.png') }}"  role="button" data-slide="next" class="arrow" >
                                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                 <span class="sr-only">Next</span>
                             </a>
@@ -57,11 +73,13 @@
         </div>
     </div>
     <style>
+        img.arrow {
+            width: 70px;
+        }
         .iframe-quiz {
-            width: 50%;
-            height: 380px;
-            padding-top: 50px;
-            margin-left: 25%;
+            width: 80%;
+            margin: auto;
+            height: 400px;
         }
 
         .btn-result {
@@ -71,12 +89,24 @@
         .carousel-control-next,.carousel-control-next:hover  {
             color: #0e111c;
         }
+        #iframe-quiz-box {
+            padding: 5%;
+        }
 
+        .carousel-inner {
+            position: initial;
+        }
+        .carousel-control-next {
+            top: initial;
+        }
         @media (max-width: 500px) {
             .iframe-quiz {
                 width: 100%;
-                height: 235px;
+                height: 400px;
                 padding-top: 20px;
+            }
+            img.arrow {
+                width: 50px;
             }
         }
     </style>
